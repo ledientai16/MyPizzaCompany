c392e50a45f19373d67ae992619809ced3bf6682
ledientai16 <ledientai16@gmail.com>
2022-05-03 22:31:16 +0700

    init

---
+++ MyPizzaCompany/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPizzaCompany.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public Boolean isComplete { get; set; }
+    }
+}
